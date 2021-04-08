afae49c0ffb773d339abc9f688b45b35f315f8d2
lundli <46197518+Lundli@users.noreply.github.com>
2021-04-08 20:14:08 +0200

    Add content to home-page & article

--- src/DataFixtures/LoadStatic_contentData.php
+++ src/DataFixtures/LoadStatic_contentData.php
@@ -11,11 +11,6 @@ class LoadStatic_contentData extends AbstractFixture
     public function load(ObjectManager $manager)
     {
         $elements = array(
-            'home-tagline' => '<p>- sender studenter til ungdomsskoler for &aring; hjelpe til som l&aelig;rerens assistent i matematikkundervisningen</p>',
-
-            'vektor_i_media' => '<p>Vektorprogrammet i media:</p><p>VG: Studenter hjelper elever med matte</p><p>NRK: Studenter hjelper elever med matte</p><p>DAGBLADET: Studenter hjelper elever med matte</p><p>AFTENPOSTEN: Studenter hjelper elever med matte</p>',
-
-
         );
         foreach ($elements as $html_id => $content) {
             $staticElement = new StaticContent();
