ef11492d93bfd163d3c0abe4ca6c2977a43e506c
lundli <46197518+Lundli@users.noreply.github.com>
2021-04-08 20:12:10 +0200

    Add content to teams-page

--- src/DataFixtures/LoadStatic_contentData.php
+++ src/DataFixtures/LoadStatic_contentData.php
@@ -13,9 +13,6 @@ class LoadStatic_contentData extends AbstractFixture
         $elements = array(
             'home-tagline' => '<p>- sender studenter til ungdomsskoler for &aring; hjelpe til som l&aelig;rerens assistent i matematikkundervisningen</p>',
 
-
-            'teams-header' => '<h1>Styre og team</h1><p>Vektorprogrammet er en stor organisasjon med assistenter i 4 norske byer. Vi trenger derfor mange frivillige bak kulissene som kan f&aring; hjulene til &aring; g&aring; rundt. Uten vektorprogrammets 13 team hadde dette aldri g&aring;tt an!</p><p>Kunne du tenkt deg et team-verv hos oss?&nbsp;<br /><strong><strong>Les mer om de ulike teamene nedenfor!</strong></strong></p>',
-
             'vektor_i_media' => '<p>Vektorprogrammet i media:</p><p>VG: Studenter hjelper elever med matte</p><p>NRK: Studenter hjelper elever med matte</p><p>DAGBLADET: Studenter hjelper elever med matte</p><p>AFTENPOSTEN: Studenter hjelper elever med matte</p>',
 
 
