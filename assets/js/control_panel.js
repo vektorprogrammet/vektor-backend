714b14843017f4fbbc9b4846063d5322925541fa
hhtjo <hahotjo@gmail.com>
2021-04-15 19:40:36 +0200

    Fix sidebar, Coreui 3.3 (#15)
    * Fix sidebar for desktop, still missing mobile support
    
    * Use c-navbar-toggler
    
    * Fix sidebar to work for mobile
    
    * Make sidebar scrollable
    
    * Remove commented code, Fix package json-error
    
    Co-authored-by: Lundli <46197518+Lundli@users.noreply.github.com>

--- assets/js/control_panel.js
+++ assets/js/control_panel.js
@@ -1,22 +1,9 @@
-$('.nav-link.active').each(function () {
-  const link = $(this);
-  link.closest('.nav-dropdown').addClass('open');
-});
-
-$('.nav-dropdown-toggle').each(function () {
-  const toggle = $(this);
-  toggle.click(function (e) {
-    if (e.target !== e.currentTarget) {
-      toggle.closest('.nav-dropdown').toggleClass('open');
-    }
-  });
-});
 
 function removeEmptyNavCategories() {
-  const navTitles = $('.nav-title');
+  const navTitles = $('.c-sidebar-nav-title');
   navTitles.each(function() {
     const title = $(this);
-    if (title.prev().hasClass('nav-title')) {
+    if (title.prev().hasClass('c-sidebar-nav-title')) {
       title.prev().remove();
     }
   });
