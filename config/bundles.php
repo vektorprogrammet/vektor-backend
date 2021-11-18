2e4e44e51e50ead909409dc1737c0a013c0215f5
Lundli <46197518+Lundli@users.noreply.github.com>
2021-11-18 18:20:41 +0100

    [VB-60] Add fm-elfinder-bundle (#87)
    * [VB-60] Add fm-elfinder-bundle
    
    * Add elfinder configurations

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
 ];
