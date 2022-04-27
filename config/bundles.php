e7e7ef4ff4fa1ebc7bd0add30a0339de442295da
Erlend Marius Ommundsen <43030358+ErlendMariusOmmundsen@users.noreply.github.com>
2022-04-27 17:38:35 +0200

    [VB-98] Upgrade to 5.4 (#130)
    * Upgrade to 5.4
    
    * [VB-98] Update doctrine/doctrine-bundle recipe
    
    * [VB-98] Update doctrine/doctrine-fixtures-bundle recipe
    
    * [VB-98] Update doctrine/doctrine-migrations-bundle recipe
    
    * [VB-98] Update friendsofsymfony/ckeditor-bundle recipe
    
    * [VB-98] Update friendsofsymfony/rest-bundle recipe
    
    * [VB-98] Update google/recaptcha recipe
    
    * [VB-98] Update liip/imagine-bundle recipe
    
    * [VB-98] Update symfony/console recipe
    
    * [VB-98] Update symfony/debug-bundle recipe
    
    * [VB-98] Update symfony/framework-bundle recipe
    
    * [VB-98] Update symfony/mailer recipe
    
    * [VB-98] Update symfony/monolog-bundle recipe
    
    * [VB-98] Update symfony/phpunit-bridge recipe
    
    * [VB-98] Update symfony/routing recipe
    
    * [VB-98] Update symfony/mailer recipe
    
    * [VB-98] Update symfony/security-bundle recipe
    
    * [VB-98] Update symfony/swiftmailer-bundle recipe
    
    * [VB-98] Update symfony/translation recipe
    
    * [VB-98] Update symfony/twig-bundle recipe
    
    * [VB-98] Update symfony/validator recipe
    
    * [VB-98] Update symfony/web-profiler-bundle recipe
    
    * [VB-98] Update additional packages(composer update)
    
    * Add symfony/runtime
    
    * Upgrade to 5.4
    
    * [VB-98] Update doctrine/doctrine-bundle recipe
    
    * [VB-98] Update doctrine/doctrine-fixtures-bundle recipe
    
    * [VB-98] Update doctrine/doctrine-migrations-bundle recipe
    
    * [VB-98] Update friendsofsymfony/ckeditor-bundle recipe
    
    * [VB-98] Update friendsofsymfony/rest-bundle recipe
    
    * [VB-98] Update google/recaptcha recipe
    
    * [VB-98] Update liip/imagine-bundle recipe
    
    * [VB-98] Update symfony/console recipe
    
    * [VB-98] Update symfony/debug-bundle recipe
    
    * [VB-98] Update symfony/framework-bundle recipe
    
    * [VB-98] Update symfony/mailer recipe
    
    * [VB-98] Update symfony/monolog-bundle recipe
    
    * [VB-98] Update symfony/phpunit-bridge recipe
    
    * [VB-98] Update symfony/routing recipe
    
    * [VB-98] Update symfony/mailer recipe
    
    * [VB-98] Update symfony/security-bundle recipe
    
    * [VB-98] Update symfony/swiftmailer-bundle recipe
    
    * [VB-98] Update symfony/translation recipe
    
    * [VB-98] Update symfony/twig-bundle recipe
    
    * [VB-98] Update symfony/validator recipe
    
    * [VB-98] Update symfony/web-profiler-bundle recipe
    
    * [VB-98] Update additional packages(composer update)
    
    * Add symfony/runtime
    
    Co-authored-by: lundli <46197518+Lundli@users.noreply.github.com>

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
