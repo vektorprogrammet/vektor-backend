b551d547af186a2f0a59ff62666c2c7ced490610
Sivert Lundli <46197518+Lundli@users.noreply.github.com>
2022-10-31 10:13:08 +0100

    VB-125 add assistant scheduling files (#200)
    * Add AssistantScheduling-files
    
    * Clean up code in AssistantScheduling-files

---
+++ src/AssistantScheduling/School.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\AssistantScheduling;
+
+use JsonSerializable;
+
+class School implements JsonSerializable
+{
+    private int $id;
+    private string $name;
+
+    /**
+     * Array: [Group, Day, Capacity]
+     */
+    private array $capacity;
+
+    /**
+     * School constructor.
+     */
+    public function __construct(array $capacity, string $name, int $id)
+    {
+        $this->capacity = $capacity;
+        $this->name = $name;
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function capacityLeftOnDay(int $group, string $day): int
+    {
+        return $this->capacity[$group][$day];
+    }
+
+    public function addAssistant(int $group, string $day)
+    {
+        --$this->capacity[$group][$day];
+    }
+
+    public function getCapacity(): array
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(array $capacity)
+    {
+        $this->capacity = $capacity;
+    }
+
+    public function isFull(): bool
+    {
+        foreach ($this->capacity as $weekDayCapacity) {
+            foreach ($weekDayCapacity as $day => $capacityLeft) {
+                if ($capacityLeft > 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return array(
+            'id' => $this->id,
+            'name' => $this->name,
+            'capacity' => $this->capacity,
+        );
+    }
+}
