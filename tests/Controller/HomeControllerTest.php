0483c24110db868619b0d82b31c267326b551658
lundli <46197518+Lundli@users.noreply.github.com>
2022-11-02 11:42:42 +0100

    Fix HomeControllerTest

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -9,8 +9,8 @@ class HomeControllerTest extends BaseWebTestCase
     public function testShow()
     {
         $crawler = $this->goTo('/');
-
-        $this->assertEquals(1, $crawler->filter('h4:contains("KVIFOR MATEMATIKK ER DET VIKTIGASTE DU")')->count());
-        $this->assertEquals(1, $crawler->filter('h2:contains("Hovedsponsorer")')->count());
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Vektorprogrammet');
+        $this->assertSelectorTextContains('h2', 'Hovedsponsorer');
     }
 }
