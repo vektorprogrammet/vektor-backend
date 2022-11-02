5d258b3040d05e490554238e3047b8158f3f9d69
lundli <46197518+Lundli@users.noreply.github.com>
2022-11-02 11:37:49 +0100

    Fix StudentsControllerTest

--- tests/Controller/StudentsControllerTest.php
+++ tests/Controller/StudentsControllerTest.php
@@ -12,11 +12,8 @@ class StudentsControllerTest extends BaseWebTestCase
 
         $crawler = $client->request('GET', '/studenter');
 
-        // Assert a specific 200 status code
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        // Assert that we have the correct amount of data
-        $this->assertEquals(1, $crawler->filter('h1:contains("Assistenter")')->count());
-        $this->assertEquals(1, $crawler->filter('p:contains("Vektorprogrammet er en studentorganisasjon som sender realfagssterke studenter")')->count());
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Assistenter');
+        $this->assertEquals(1, $crawler->filter('p:contains("Vektorprogrammet er en studentorganisasjon som")')->count());
     }
 }
