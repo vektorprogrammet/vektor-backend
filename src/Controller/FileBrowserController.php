29fd39726bb8e57e2bb0ebd4e85640ca5de89c9e
lundli <46197518+Lundli@users.noreply.github.com>
2022-11-03 11:48:25 +0100

    Cleanup routing

--- src/Controller/FileBrowserController.php
+++ src/Controller/FileBrowserController.php
@@ -38,39 +38,4 @@ class FileBrowserController extends BaseController
         return new BinaryFileResponse($prefix);
     }
 
-    /**
-     * This controller renders and returns a page with links to all the files in the public folder (the same folder
-     * shown under "Offentlige filer" in the file browser. At delivery there isn't actually a link to this page
-     * on the website. But both this controller and the twig template should be ready to use.
-     *
-     * @param $folder
-     *
-     * @return Response
-     */
-    public function showPublicFiles($folder): Response
-    {
-        //Read the public folder from paramters.yml
-        $publicFolder = $this->container->getParameter('public_uploads');
-        //Create the path that should be scanned
-        if ($folder == 'all') {
-            $path = $publicFolder;
-        } else {
-            $path = $publicFolder.'/'.$folder;
-        }
-        //Scan the folder for files, exclude hidden files (those that starts with dot)
-        $files = preg_grep('/^([^.])/', scandir($path));
-        //Prepend the path to each file &=call by reference
-        foreach ($files as &$file) {
-            $file = $path.'/'.$file;
-        }
-        //Make an array without directories included.
-        $filesWithoutFolders = array();
-        foreach ($files as $file) {
-            if (!is_dir($file)) {
-                $filesWithoutFolders[] = $file;
-            }
-        }
-        //Render the twig
-        return $this->render('file_system/public_files.html.twig', array('files' => $filesWithoutFolders));
-    }
 }
