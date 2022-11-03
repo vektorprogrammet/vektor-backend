f52ad3faf0de128bd5632bf8438db420b1d57914
lundli <46197518+Lundli@users.noreply.github.com>
2022-11-03 21:54:02 +0100

    Remove redundant gender data in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,14 +22,9 @@ class HomeController extends BaseController
         $closestDepartment = $geoLocation->findNearestDepartment($departments);
         $ipWasLocated = $geoLocation->findCoordinatesOfCurrentRequest();
 
-        $femaleAssistantCount = $this->getDoctrine()->getRepository(AssistantHistory::class)->numFemale();
-        $maleAssistantCount = $this->getDoctrine()->getRepository(AssistantHistory::class)->numMale();
-
         return $this->render('home/index.html.twig', [
             'assistantCount' => $assistantsCount + 600, // + Estimated number of assistants not registered in website
             'teamMemberCount' => $teamMembersCount + 160, // + Estimated number of team members not registered in website
-            'femaleAssistantCount' => $femaleAssistantCount,
-            'maleAssistantCount' => $maleAssistantCount,
             'ipWasLocated' => $ipWasLocated,
             'departmentsWithActiveAdmission' => $departmentsWithActiveAdmission,
             'closestDepartment' => $closestDepartment,
