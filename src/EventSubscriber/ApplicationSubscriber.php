23fbbef2eba5d142b8414654d5feea7920ad1316
Lundli <46197518+Lundli@users.noreply.github.com>
2022-11-11 11:09:50 +0100

    Add EventSubscriber files

---
+++ src/EventSubscriber/ApplicationSubscriber.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace AppBundle\EventSubscriber;
+
+use App\Event\ApplicationCreatedEvent;
+use App\Service\AdmissionNotifier;
+use App\Mailer\MailerInterface;
+use App\Service\UserRegistration;
+use Exception;
+use Swift_Message;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Twig\Environment;
+
+class ApplicationSubscriber implements EventSubscriberInterface
+{
+    private MailerInterface $mailer;
+    private Environment $twig;
+    private AdmissionNotifier $admissionNotifier;
+    private UserRegistration $userRegistrationService;
+
+    /**
+     * ApplicationAdmissionSubscriber constructor.
+     */
+    public function __construct(MailerInterface $mailer, Environment $twig, AdmissionNotifier $admissionNotifier, UserRegistration $userRegistrationService)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->admissionNotifier = $admissionNotifier;
+        $this->userRegistrationService = $userRegistrationService;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ApplicationCreatedEvent::NAME => array(
+                array( 'sendConfirmationMail', 0 ),
+                array( 'createAdmissionSubscriber', - 2 ),
+            ),
+        );
+    }
+
+    public function createAdmissionSubscriber(ApplicationCreatedEvent $event)
+    {
+        $application = $event->getApplication();
+        $department = $application->getUser()->getDepartment();
+        $email = $application->getUser()->getEmail();
+        try {
+            $this->admissionNotifier->createSubscription($department, $email, true);
+        } catch (Exception $e) {
+            // Ignore
+        }
+    }
+
+    public function sendConfirmationMail(ApplicationCreatedEvent $event)
+    {
+        $application = $event->getApplication();
+        $user = $application->getUser();
+        $newUserCode = null;
+        if (!$user->getPassword()) {
+            $newUserCode = $this->userRegistrationService->setNewUserCode($user);
+        }
+
+        $template = 'admission/admission_email.html.twig';
+        if ($application->getUser()->hasBeenAssistant()) {
+            $template = 'admission/admission_existing_email.html.twig';
+        }
+
+        // Send a confirmation email with a copy of the application
+        $emailMessage = (new Swift_Message())
+            ->setSubject('Søknad - Vektorassistent')
+            ->setReplyTo($application->getDepartment()->getEmail())
+            ->setTo($application->getUser()->getEmail())
+            ->setBody($this->twig->render($template, array(
+                'application'   => $application,
+                'new_user_code' => $newUserCode
+            )), 'text/html');
+
+        $this->mailer->send($emailMessage);
+    }
+}
