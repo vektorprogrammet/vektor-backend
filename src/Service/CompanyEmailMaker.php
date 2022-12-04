6cf8a85bb3637621427422f3d81791e3ada7b8fb
Sivert Lundli <46197518+Lundli@users.noreply.github.com>
2022-12-04 20:27:36 +0100

    VB-151 event subscriber files (#246)
    * add files
    
    * Add EventSubscriber files
    
    * sonarcloud fixes
    
    * sonarcloud fixes
    
    * fix indent
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes

---
+++ src/Service/CompanyEmailMaker.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CompanyEmailMaker
+{
+    const EMAIL_DOMAIN = '@vektorprogrammet.no';
+    private EntityManagerInterface $em;
+    private LogService $logger;
+
+    /**
+     * CompanyEmailMaker constructor
+     */
+    public function __construct(EntityManagerInterface $em, LogService $logger)
+    {
+        $this->em = $em;
+        $this->logger = $logger;
+    }
+
+    public function setCompanyEmailFor(User $user, $blackList): ?string
+    {
+        $allCompanyEmails = $this->em->getRepository(User::class)->findAllCompanyEmails();
+        $allEmails = array_merge($allCompanyEmails, $blackList);
+        $firstName = strtolower($this->replaceNorwegianCharacters($user->getFirstName()));
+        $fullName = strtolower($this->replaceNorwegianCharacters($user->getFullName()));
+
+
+        // self::EMAIL_DOMAIN is constant @vektorprogrammet.no
+        $email = preg_replace('/\s+/', '.', $firstName) . self::EMAIL_DOMAIN;
+        if (array_search($email, $allEmails) !== false) {
+            $email = preg_replace('/\s+/', '.', $fullName) . self::EMAIL_DOMAIN;
+        }
+
+        $i = 2;
+        while (array_search($email, $allEmails) !== false) {
+            $email = preg_replace('/\s+/', '.', $fullName) . $i . self::EMAIL_DOMAIN;
+            $i++;
+        }
+
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->logger->alert("Failed to create email for $user. Invalid email: $email");
+            return null;
+        }
+
+        $user->setCompanyEmail($email);
+        $this->em->flush();
+        $this->logger->info("Created company email, $email, for $user");
+        return $email;
+    }
+
+    private function replaceNorwegianCharacters($string)
+    {
+        setlocale(LC_ALL, 'nb_NO');
+
+        // Converts accents and norwegian characters
+        $string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+
+        // Removes ' and `after iconv(), and other invalid characters
+        $string = preg_replace("/[^A-Za-z0-9 ]/", '', $string);
+        return $string;
+    }
+}
