39769f8ec39656bf2b75e7be17315727b61f0d1c
Sivert Lundli <46197518+Lundli@users.noreply.github.com>
2022-12-15 09:20:50 +0100

    refactor

--- src/Role/Roles.php
+++ src/Role/Roles.php
@@ -4,15 +4,15 @@ namespace App\Role;
 
 class Roles
 {
-    public const ASSISTANT = 'ROLE_USER';
-    public const TEAM_MEMBER = 'ROLE_TEAM_MEMBER';
-    public const TEAM_LEADER = 'ROLE_TEAM_LEADER';
-    public const ADMIN = 'ROLE_ADMIN';
+    final public const ASSISTANT = 'ROLE_USER';
+    final public const TEAM_MEMBER = 'ROLE_TEAM_MEMBER';
+    final public const TEAM_LEADER = 'ROLE_TEAM_LEADER';
+    final public const ADMIN = 'ROLE_ADMIN';
 
-    public const ALIAS_ASSISTANT = 'assistant';
-    public const ALIAS_TEAM_MEMBER = 'team_member';
-    public const ALIAS_TEAM_LEADER = 'team_leader';
-    public const ALIAS_ADMIN = 'admin';
+    final public const ALIAS_ASSISTANT = 'assistant';
+    final public const ALIAS_TEAM_MEMBER = 'team_member';
+    final public const ALIAS_TEAM_LEADER = 'team_leader';
+    final public const ALIAS_ADMIN = 'admin';
 
     public static function GetRoleName($role): string
     {
