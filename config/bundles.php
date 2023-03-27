b897ecc204650f6388e26cd8bb7cb426cdabda90
Filip Skaug <filis@stud.ntnu.no>
2023-03-27 16:22:41 +0200

    update composer.lock

--- config/bundles.php
+++ config/bundles.php
@@ -21,6 +21,6 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
