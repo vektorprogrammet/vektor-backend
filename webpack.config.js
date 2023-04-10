d0e47d5085e34b821a76e0bd6b7f4e27b42dc707
Sivert Lundli <46197518+Lundli@users.noreply.github.com>
2023-04-10 17:15:31 +0200

    div

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,19 @@ Encore
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: '../images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        // to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+        })
+
     /*
      * ENTRY CONFIG
      *
@@ -21,6 +34,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('pages/teacher', './assets/styles/pages/teacher.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
