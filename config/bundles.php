b4be02db6e3e0bdb3b9f09e80451aec25c2068bd
Sivert Lundli <46197518+Lundli@users.noreply.github.com>
2023-04-10 11:30:10 +0200

    install webpack

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
