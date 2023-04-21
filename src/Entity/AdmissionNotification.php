6bff79c6e8a81cfc29336fd17f8923fc96b8fa73
Sivert Lundli <46197518+Lundli@users.noreply.github.com>
2023-04-21 20:28:53 +0200

    fix cap

--- src/Entity/AdmissionNotification.php
+++ src/Entity/AdmissionNotification.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 class AdmissionNotification
 {
     #[ORM\Id]
-    #[ORM\Column(type: TYPES::INTEGER)]
+    #[ORM\Column(type: Types::INTEGER)]
     #[ORM\GeneratedValue(strategy: 'AUTO')]
     private ?int $id = null;
 
