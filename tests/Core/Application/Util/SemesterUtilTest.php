6d3da748d90b10e08a7f02576553a9a083cda749
Hans Tjøtta <hahotjo@gmail.com>
2023-10-29 10:36:32 +0000

    Fix SemesterUtilTest

--- tests/Core/Application/Util/SemesterUtilTest.php
+++ tests/Core/Application/Util/SemesterUtilTest.php
@@ -5,6 +5,7 @@ namespace Tests\Core\Application\Util;
 use App\Core\Application\Util\SemesterUtil;
 use App\Core\Domain\Entity\Semester;
 use PHPUnit\Framework\TestCase;
+use DateTime;
 
 
 class SemesterUtilTest extends TestCase
@@ -12,22 +13,40 @@ class SemesterUtilTest extends TestCase
 
     public function testTimeToSemester(): void
     {
-        $semester = SemesterUtil::timeToSemester(new \DateTime('2020-09-01'));
-        $this->assertEquals(new Semester(1, 2020, 1), $semester);
+        $semester = SemesterUtil::timeToSemester(new DateTime('2020-09-01'));
+        $actualSemester = new Semester();
+        $actualSemester->setSemesterTime('Høst');
+        $actualSemester->setYear('2020');
+        $this->assertEquals($actualSemester, $semester);
 
-        $semester = SemesterUtil::timeToSemester(new \DateTime('2020-12-31'));
-        $this->assertEquals(new Semester(2, 2020, 2), $semester);
+        $semester = SemesterUtil::timeToSemester(new DateTime('2020-12-31'));
+        $actualSemester = new Semester();
+        $actualSemester->setSemesterTime('Høst');
+        $actualSemester->setYear('2020');
+        $this->assertEquals($actualSemester, $semester);
 
-        $semester = SemesterUtil::timeToSemester(new \DateTime('2021-01-01'));
-        $this->assertEquals(new Semester(3, 2021, 1), $semester);
+        $semester = SemesterUtil::timeToSemester(new DateTime('2021-01-01'));
+        $actualSemester = new Semester();
+        $actualSemester->setSemesterTime('Vår');
+        $actualSemester->setYear('2021');
+        $this->assertEquals($actualSemester, $semester);
 
-        $semester = SemesterUtil::timeToSemester(new \DateTime('2021-08-31'));
-        $this->assertEquals(new Semester(4, 2021, 1), $semester);
+        $semester = SemesterUtil::timeToSemester(new DateTime('2021-08-31'));
+        $actualSemester = new Semester();
+        $actualSemester->setSemesterTime('Høst');
+        $actualSemester->setYear('2021');
+        $this->assertEquals($actualSemester, $semester);
 
         $semester = SemesterUtil::timeToSemester(new \DateTime('2021-09-01'));
-        $this->assertEquals(new Semester(5, 2021, 2), $semester);
+        $actualSemester = new Semester();
+        $actualSemester->setSemesterTime('Høst');
+        $actualSemester->setYear('2021');
+        $this->assertEquals($actualSemester, $semester);
 
         $semester = SemesterUtil::timeToSemester(new \DateTime('2021-12-31'));
-        $this->assertEquals(new Semester(6, 2021, 2), $semester);
+        $actualSemester = new Semester();
+        $actualSemester->setSemesterTime('Høst');
+        $actualSemester->setYear('2021');
+        $this->assertEquals($actualSemester, $semester);
     }
 }
